06d4007b370516312cffd03e5edf450ab97dc9e9
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2024-06-18 19:38:15 +0530

    :rocket: Footer

---
+++ src/data/index.js
@@ -0,0 +1,18 @@
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      link:"https://github.com/Nishu0/arweave-project",
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      link:"https://x.com/itznishuu_",
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      link:"https://www.linkedin.com/in/Nishu0/",
+      img: "/link.svg",
+    },
+  ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { ConnectButton, useConnection } from "@arweave-wallet-kit/react";
 import Header from "../components/Header";
 import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 const Home = () => {
   const { connected } = useConnection();
@@ -10,6 +11,7 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className="max-w-7xl w-full">
         <Hero />
+        <Footer />
         </div>
     </main>
   );
