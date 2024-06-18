ac399c62074766bae8c6e30378497fd79f58e4da
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2024-06-18 16:20:02 +0530

    :zap: Base Vite App with Wallet Support

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import { ConnectButton } from "@arweave-wallet-kit/react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const navLinks = [
+    {
+      title: "View",
+      path: "/view",
+    },
+    {
+      title: "Create",
+      path: "/create",
+    },
+  ];
+  return (
+    <header className="bg-white py-2 px-4 flex justify-between items-center shadow-md">
+      <Link to="/" className="text-black no-underline">
+        <h1 className="m-0">BlinkBlog</h1>
+      </Link>
+      <div>
+        {navLinks.map((link) => {
+          return (
+            <Link key={link.title} to={link.path} className="text-black no-underline mx-2">
+              {link.title}
+            </Link>
+          );
+        })}
+      </div>
+      <ConnectButton
+        profileModal={true}
+        showBalance={false}
+        showProfilePicture={true}
+      />
+    </header>
+  );
+};
+
+export default Header;
