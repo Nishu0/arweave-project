94171b862c9685d44937d772b9ece1dbd3c1a108
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2024-06-19 02:52:49 +0530

    :tada: UI Features Added

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,34 @@
-  
+export const workExperience = [
+  {
+    id: 1,
+    title: "Developer Assistance ",
+    desc: "Get round-the-clock support from experienced Arweave developers. Whether you're troubleshooting code, integrating Arweave into your project.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Stay Informed with Community News",
+    desc: "Never miss an update with real-time news and insights from the Arweave community. Stay connected with the latest developments, network with peers.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Comprehensive Arweave Solutions",
+    desc: "Find answers to all your Arweave-related questions. Our platform offers a vast repository of solutions, from basic troubleshooting to advanced use cases, ensuring you have the information you need to succeed.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Developer Expertise",
+    desc: "Benefit from the knowledge and experience of top Arweave developers. Get detailed, expert-driven answers to your queries.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
   export const socialMedia = [
     {
       id: 1,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import { Link } from "react-router-dom";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import Experience from "../components/Experience";
 
 const Home = () => {
   const { connected } = useConnection();
@@ -11,6 +12,7 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className="max-w-7xl w-full">
         <Hero />
+        <Experience />
         <Footer />
         </div>
     </main>
